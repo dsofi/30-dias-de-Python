de3205f024c00e097d1bf683f0d4b1061c3769e0
Sofi <dornasofia@gmail.com>
2023-03-04 21:50:35 -0300

    Avances día 26

--- -apuntes/26-python-for-web-apunte.py
+++ -apuntes/26-python-for-web-apunte.py
@@ -53,3 +53,53 @@
 
 # Creating routes
 # The home route.
+'''
+# let's import the flask
+from flask import Flask
+import os # importing operating system module
+
+app = Flask(__name__)
+
+@app.route('/') # this decorator create the home route
+def home ():
+    return '<h1>Welcome</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About us</h1>'
+
+
+if __name__ == '__main__':
+    # for deployment we use the environ
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+'''
+
+# To run the flask application, write python app.py in the main flask application directory.
+# After you run python app.py check local host 5000.
+# Let us add additional route. Creating about route
+
+# Now, we added the about route in the above code. How about if we want to render an HTML file instead of string? It is possible to render HTML file using the function render_templae. Let us create a folder called templates and create home.html and about.html in the project directory. Let us also import the render_template function from flask.
+
+
+
+# Creating templates
+# Create the HTML files inside templates folder.
+
+# home.html (return render_template('home.html'))
+# about.html (return render_template('about.html'))
+
+# As you can see to go to different pages or to navigate we need a navigation. Let's add a link to each page or let's create a layout which we use to every page.
+'''
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/about">About</a></li>
+</ul>
+'''
+
+
+# Now, we can navigate between the pages using the above link. Let us create additional page which handle form data. You can call it any name, I like to call it post.html.
+
+# We can inject data to the HTML files using Jinja2 template engine.
+
